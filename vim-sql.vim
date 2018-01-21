3015798c5177a7670232b9748810a1f24b398fa8
Ron Kuslak <rkuslak@outlook.com>
2018-01-21 15:55:30 -0600

    More work to make it display based on Vim buffer status

--- vim-sql.vim
+++ vim-sql.vim
@@ -11,48 +11,25 @@
 "    as eventual addition of other engines (such as Postgre or MariaDB) is a
 "    long-term goal, after a first-class MS SQL experience is created.
 
+if !has("python3") == 1
+    echo "Python 3 support is required. You can test this in Vim by calling ':has(\"Python3\")'"
+endif
+
 " Root path to this file:
 let g:vim_sql_script_path = expand('<sfile>:p:h')
 
-function! s:VimSqlInit()
-    " Do 1-time initialization functions in Vim to sanitize enviroment
-    if !exists("g:VimSqlInitialized")
-        py3 << py_eof
+" Set import path to include plugin directory.
+py3 << py3eof
 import sys
 import vim
 
+# Add Vim script path to module path so we can actually load:
 VIM_SQL_SCRIPT_PATH = vim.eval("g:vim_sql_script_path")
-if not VIM_SQL_SCRIPT_PATH in sys.path:
+if VIM_SQL_SCRIPT_PATH not in sys.path:
     sys.path += [VIM_SQL_SCRIPT_PATH]
+py3eof
 
-# TODO: Is this needed? Shouldn't need the consuming code to know of the DBs or
-#   models?
-py_eof
-        py3file Z:\vim-sql\test.py
-        " let g:VimSqlInitialized = 1
-    endif
-endfunction
+exec "py3file ". g:vim_sql_script_path. "\\vimbackend.py"
 
-call s:VimSqlInit()
-
-function! s:VimSqlGetBuffer(buffertype)
-    let calling_buff = bufnr("%")
-    let calling_buff_name = bufname(calling_buff)
-    
-endfunction
-
-function! VimSqlShowServers()
-    " Display the server list for the currently active buffer via a vsplit
-    let current_buff = bufnr("%")
-    " let buffer_name = 'Servers: '
-    let buffer_name = bufname(current_buff)
-    let server_buffer = getbufvar(current_buff, "vimsqlserverlist")
-    let server_buffer_name = "Database: ". buffer_name
-    echo current_buff
-    echo buffer_name
-    echo server_buffer_name
-    exec ":b ". current_buff
-endfunction
-
-command! VimSqlServers exec VimSqlShowServers()
-
+" Add commands we can keybind, to make life easier:
+command! VimSqlServers py3 vimbackend.show_database_list() 
--- vimbackend.py
+++ vimbackend.py
@@ -6,40 +6,78 @@
     information on SQL connections.
 
     Intended to be used with models returned from the vim_sql.models class.
+
+    TODO: Convert to a class to keep things out of the global namespace.
 '''
+import db.mssql
 import vim
 
 
-def createscratchbuffer(parentname):
-    ''' Create a Vim 'results' scratch buffer for the buffer named in
-        parentname.
+class vimbackend(object):
+    @staticmethod
+    def createscratchbuffer(parentname):
+        ''' Create a Vim 'results' scratch buffer for the buffer named in
+            parentname.
 
-        if creating, use:
-            :new
-            :setlocal buftype=nofile
-            :setlocal bufhidden=hide
-            :setlocal noswapfile
-            :autocmd BufEnter results_buffer_name stopinsert
-    '''
-    pass
+            if creating, use:
+                :new
+                :setlocal buftype=nofile
+                :setlocal bufhidden=hide
+                :setlocal noswapfile
+                :autocmd BufEnter results_buffer_name stopinsert
+        '''
+        pass
 
+    @staticmethod
+    def updated_database_list(databases):
+        ''' Takes passed list of db.models objects and creates a plesant
+            textual view of them.
+        '''
+        results = ""
 
-def getscratchbuffer(parentname):
-    ''' Returns the buffer object for the buffer named "parentname" or None if
-        no such buffer exists.
-    '''
-    for buffer in vim.buffers:
-        if buffer.name == "results_" + parentname:
-            return buffer
+        def isactive(state):
+            ''' Returns formatted string based on if passed state is active or
+                not
+            '''
+            if state:
+                return "+ "
+            return "  "
 
-    return None
+        for database in databases:
+            results += isactive(database.active) + database.name + "\n"
+            if database.active:
+                # Display table records:
+                for table in database.tables:
+                    results += "  {}{}\n".format(isactive(table.active),
+                                                 table.name)
+                    if table.active:
+                        for column in table.columns:
+                            results += "      {} [{}]\n".format(column.name,
+                                                                column.sqltype)
 
+        return results
 
-def isactive(state):
-    ''' Returns formatted string based on if passed state is active or not '''
-    if state:
-        return "-↘ "
-    return "  "
+    @staticmethod
+    def show_database_list():
+        ''' Sets (or creates if non-existant) the contents of the database list
+            buffer for the currently selected buffer to the values held in a
+            query against the database.
+        '''
+        buffer = vim.current.buffer
+
+        # Attempt to find a conncetion for this buffer in the global cache:
+        if buffer.number not in VIM_SQL_CONNECTIONS.keys():
+            server = getconfigvar("vim_sql_server")
+            database = getconfigvar("vim_sql_database")
+            username = getconfigvar("vim_sql_username")
+            password = getconfigvar("vim_sql_password")
+            connection = db.mssql.sqlrunner(server, database, username,
+                                            password)
+            VIM_SQL_CONNECTIONS[buffer.number] = connection
+
+        connection = VIM_SQL_CONNECTIONS[buffer.number]
+        print(formatdatabaselist(connection.getdatabases()))
+
 
 
 def formatdatabaselist(databases):
@@ -53,7 +91,8 @@ def formatdatabaselist(databases):
         if database.active:
             # Display table records:
             for table in database.tables:
-                results += "  {}{}\n".format(isactive(table.active), table.name)
+                results += "  {}{}\n".format(isactive(table.active),
+                                             table.name)
                 if table.active:
                     for column in table.columns:
                         results += "      {} [{}]\n".format(column.name,
@@ -62,18 +101,54 @@ def formatdatabaselist(databases):
     return results
 
 
+VIM_SQL_CONNECTIONS = {}
+VIM_SQL_LIST_BUFFERS = {}
+VIM_SQL_RESULTS_BUFFERS = {}
+
+
+def getconfigvar(var):
+    ''' Searches the Vim variables for first the buffer, then global, for
+        passed variable name. Returns None if not found.
+    '''
+    return vim.current.buffer.vars.get(var) or vim.vars.get(var) or None
+
+
+def getlistbufferforbuffer(bufn):
+    ''' Returns the database list for a buffer passed, if a editor window, or
+        for the connection if passed a list or results buffer
+    '''
+    if bufn in VIM_SQL_CONNECTIONS.keys():
+        return VIM_SQL_CONNECTIONS[bufn]
+
+    if bufn in VIM_SQL_LIST_BUFFERS.values():
+        return bufn
+
+    if bufn in VIM_SQL_RESULTS_BUFFERS.values():
+        # It is a results buffer, find the parent buffer and return list buffer
+        # if it exists:
+        for key in VIM_SQL_RESULTS_BUFFERS.keys():
+            if VIM_SQL_RESULTS_BUFFERS[key] == bufn:
+                return getlistbufferforbuffer(key)
+
+    return None
+
+
 def currentbufferdatabases():
+    ''' Sets (or creates if non-existant) the contents of the database list
+        buffer for the currently selected buffer to the values held in a query
+        against the database.
+    '''
     buffer = vim.current.buffer
-    try:
-        buffer.connection
-    except:
-        buffer.connection = None
 
-    if not buffer.connection:
-        server = buffer.vars.get("vim_sql_server") or None
-        database = buffer.vars.get("vim_sql_database") or None
-        username = buffer.vars.get("vim_sql_username") or None
-        password = buffer.vars.get("vim_sql_password") or None
-        buffer.connection = db.mssql.sqlrunner(server, database, username, password)
+    # Attempt to find a conncetion for this buffer in the global cache:
+    if buffer.number not in VIM_SQL_CONNECTIONS.keys():
+        print("Creating buffer")
+        server = getconfigvar("vim_sql_server")
+        database = getconfigvar("vim_sql_database")
+        username = getconfigvar("vim_sql_username")
+        password = getconfigvar("vim_sql_password")
+        connection = db.mssql.sqlrunner(server, database, username, password)
+        VIM_SQL_CONNECTIONS[buffer.number] = connection
 
-    print(formatdatabaselist(buffer.connection.getdatabases()))
+    connection = VIM_SQL_CONNECTIONS[buffer.number]
+    print(formatdatabaselist(connection.getdatabases()))
